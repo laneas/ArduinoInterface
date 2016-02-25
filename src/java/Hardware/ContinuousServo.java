530a6080d68cc87adfc12157f32e2c7108fddbf7
laneas <ardjenlane@hotmail.com>
2016-02-25 15:21:27 -0500

    Merged Arduino Hardware and Communication Classes with the start of the Web Interface.

---
+++ src/java/Hardware/ContinuousServo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hardware;
+
+/**
+ *
+ * @author Ardjen
+ */
+public class ContinuousServo extends Servo implements Incrementable
+{
+    
+    public ContinuousServo(int thePin)
+    {
+        super(thePin);
+        upperLimit = 180;
+        lowerLimit = 0;
+        defaultAngle = 90;
+    }
+    
+}
